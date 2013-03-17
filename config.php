05c598ef8968fbccf9942184527a23d2dccc1bfa
Kevin <github@kevingimbel.de>
2013-03-17 18:05:23 +0100

    Again trying to upload.

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+	$root = 'http://v6.kevingimbel.de/';
+
+	$subfolder = 'portfolio/';
+	$subfolder_foto = 'fotografie/';
+	$subfolder_web = 'webdesign/';
+
+?>
---
+++ footer.php
@@ -0,0 +1,24 @@
+</section>
+<footer class="site-footer h150 clearfix">
+	<div class="col w50">
+		<ul class="footer-links">
+			<li><span class="entypo-link"></span> <a href="http://codepen.io/kevingimbel">codepen.io</a></li>
+			<li><span class="entypo-twitter"><span> <a href="http://twitter.com/_kevinatari">Twitter</a></li>
+			<li><span class="entypo-facebook-circled"></span> <a href="http://facebook.com/kevingimbel">Facebook</a></li>
+			<li><span class="entypo-globe"></span> <a href="http://atarijunge.de">Blog</a></li>
+		</ul>
+		<p>&copy; 2013 Kevin Gimbel</p>
+	</div>
+
+	<div class="col w50">
+		<p>Kevin Gimbel <br />
+			Goldbornstraße 6<br />
+			65439 Flörsheim am Main</p>
+		<p><a href="mailto:hallo@kevingimbel.de">hallo@kevingimbel.de</a></p>
+	</div>
+</footer>
+
+</div>	<!-- /wrapper -->
+	
+</body>
+</html>
