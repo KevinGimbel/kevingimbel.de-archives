29d16ecd43b8ada58c4660d3a0250ab24277aebd
Kevin <github@kevingimbel.de>
2013-03-17 18:23:57 +0100

    Files for v6.

---
+++ portfolio/fotografie/folklore.php
@@ -0,0 +1,13 @@
+<?php include('http://v6.kevingimbel.de/header.php'); ?>
+<span><h1>Folklore</h1></span>
+<p>Wiesbaden, 2010</p>
+<img src="http://daten.kevingimbel.de/img/portfolio/live/folklore/Blumentopf-8600.jpg"/>
+<img src="http://daten.kevingimbel.de/img/portfolio/live/folklore/Blumentopf-8635.jpg"/>
+<img src="http://daten.kevingimbel.de/img/portfolio/live/folklore/Blumentopf-8746.jpg"/>
+<img src="http://daten.kevingimbel.de/img/portfolio/live/folklore/Blumentopf-8750.jpg"/>
+<img src="http://daten.kevingimbel.de/img/portfolio/live/folklore/Blumentopf-8804.jpg"/>
+<img src="http://daten.kevingimbel.de/img/portfolio/live/folklore/Frittenbude-0086.jpg"/>
+<img src="http://daten.kevingimbel.de/img/portfolio/live/folklore/Frittenbude-0339.jpg"/>
+<img src="http://daten.kevingimbel.de/img/portfolio/live/folklore/Frittenbude-0427.jpg"/>
+<img src="http://daten.kevingimbel.de/img/portfolio/live/folklore/Frittenbude-0446.jpg"/>
+<?php include('http://v6.kevingimbel.de/footer.php'); ?>
