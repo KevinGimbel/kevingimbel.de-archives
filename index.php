de27712609300ea509e24198dc9ae092de062653
Kevin <github@kevingimbel.de>
2013-03-17 18:08:10 +0100

    v6

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php include('header.php'); ?>
+
+			<span>
+				<h1><a href="/portfolio/fotografie/sw">Schwarz-Weiß Fotografie</a></h1>
+			</span>
+			<img src="http://daten.kevingimbel.de/img/portfolio/live/gc2012/FALD_BW.jpg" />
+
+
+<?php include('footer.php'); ?>
